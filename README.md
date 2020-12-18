7b9ce28ee0a9905065583aac596fb4987e5a8743
emredv <33000794+emredv@users.noreply.github.com>
2020-12-18 16:19:40 +0300

    v1

--- README.md
+++ README.md
@@ -1,2 +1,120 @@
-# Veyosis-PHP-Client
-Veyosis API Servisleri PHP Kütüphanesi
+# Veyosis (veyosis.com) PHP API Client  
+veyosis.com API servisleri için PHP API kütüphanesi.
+
+#### AUTH
+```php
+$veyosis = new Veyosis($token);
+/* veya */
+Veyosis::auth($token);
+```
+`$token`: Veyosis API Kodu *  
+
+---
+
+## MARKA İŞLEMLERİ  
+#### Marka Listeleme  
+```php
+Veyosis::brand()->get();
+```
+
+---
+
+## İZİN YÖNETİMİ  
+#### Tekil İzin Ekleme  
+```php
+Veyosis::consent()->single($brand,$type,$recipientType,$recipient,$source,$consentDate,$status);
+```
+`$brand`: İzin eklemek istediğiniz marka kodu *  
+`$type`: İzin tipi (ARAMA/MESAJ/EPOSTA) *  
+`$recipientType`: Alıcı tipi (BIREYSEL/TACIR) *  
+`$recipient`: Alıcı *  
+`$source`: İzin kaynağı (HS_) *  
+`$consentDate`: İzin tarihi *  
+`$status`: İzin durumu (ONAY/RET) *  
+
+---
+
+#### Çoklu İzin Ekleme (Asenkron)
+```php
+Veyosis::consent()->async($brand,$recipients);
+```
+`$brand`: İzin eklemek istediğiniz marka kodu *  
+`$recipients`:
+```php
+[
+  [
+    'recipientType' => '',
+    'type' => '',
+    'recipient' => '',
+    'source' => '',
+    'consentDate' => '',
+    'status' => ''
+  ],
+  .
+  .
+  .
+]
+```
+
+---
+
+#### Çoklu İzin Ekleme Durumu
+```php
+Veyosis::consent()->status($transaction);
+```
+`$transaction`: Çoklu izin isteğinde dönen işlem kodu *  
+
+---
+
+## İZİN SORGULAMA  
+#### Tekil İzin Sorgulama  
+```php
+Veyosis::report()->single($brand,$type,$recipientType,$recipient);
+```
+`$brand`: İzin eklemek istediğiniz marka kodu *  
+`$type`: İzin Tipi (ARAMA/MESAJ/EPOSTA) *  
+`$recipientType`: Alıcı Tipi (BIREYSEL/TACIR) *  
+`$recipient`: Alıcı *  
+
+---
+
+#### Çoklu İzin Sorgulama (Asenkron)
+```php
+Veyosis::consent()->async($brand,$recipients);
+```
+`$brand`: İzin eklemek istediğiniz marka kodu *  
+`$recipients`:
+```php
+[
+  [
+    'type' => '',
+    'recipientType' => '',
+    'recipient' => ''
+  ],
+  .
+  .
+  .
+]
+```
+
+---
+
+#### Çoklu İzin Sorgulama Durumu
+```php
+Veyosis::report()->status($transaction);
+```
+`$transaction`: Çoklu izin isteğinde dönen işlem kodu *  
+
+---
+
+#### DİĞER PARAMETRELER  
+```php
+Veyosis::get()->error()->code; // Alınan son hatayı getirir.
+Veyosis::get()->error()->desc; // Alınan son hata açıklamasını getirir.
+
+Veyosis::get()->request()->body; // Yapılan son isteği getirir.
+Veyosis::get()->request()->url; // Yapılan son istek url adresini getirir.
+
+Veyosis::get()->result()->code; // Yapılan son istek cevabının http kodunu getirir.
+Veyosis::get()->result()->body; // Yapılan son istek cevabını adresini getirir.
+```
--- src/Veyosis.php
+++ src/Veyosis.php
@@ -1,1 +1,98 @@
 <?php
+class Veyosis {
+  protected static $_url = "https://api.veyosis.com/";
+  protected static $_token = null;
+  protected static $_arr = [];
+  protected static $_api = [];
+  const ERROR = "api_error";
+  const ERROR_MSG = "api_error_msg";
+  const REQUEST = "api_request";
+  const REQUEST_URL = "api_request_url";
+  const REQUEST_HEADER = "api_request_header";
+  const RESULT = "api_result";
+  const RESULT_CODE = "api_result_code";
+
+  function __construct($token=null){
+    if($token!=null) self::auth($token);
+  }
+
+  public static function auth($token){
+    self::$_token = $token;
+    return true;
+  }
+
+  public static function brand(){
+    return new class{
+      function get(){ return Veyosis::call("brand/get"); }
+    };
+  }
+
+  public static function consent(){
+    return new class{
+      function single($brand,$type,$recipientType,$recipient,$source,$consentDate,$status){
+        return Veyosis::call("consent/single/$brand",[
+          'recipient' => $recipient,
+          'type' => $type,
+          'recipientType' => $recipientType,
+          'recipient' => $recipient,
+          'source' => $source,
+          'consentDate' => $consentDate,
+          'status' => $status]); }
+      function async($brand,$recipients){ return Veyosis::call("consent/async/$brand",$recipients); }
+      function status($transaction){ return Veyosis::call("consent/status/$transaction"); }
+    };
+  }
+
+  public static function report(){
+    return new class{
+      function single($brand,$type,$recipientType,$recipient){
+        return Veyosis::call("report/single/$brand",[
+          'recipient' => $recipient,
+          'type' => $type,
+          'recipientType' => $recipientType]); }
+      function async($brand,$recipients){ return Veyosis::call("report/async/$brand",$recipients); }
+      function status($transaction){ return Veyosis::call("report/status/$transaction"); }
+    };
+  }
+
+  public static function call($action, $data=[]){
+    self::$_arr[self::ERROR] = false;
+    self::$_arr[self::ERROR_MSG] = false;
+    $url = self::$_url.$action;
+    self::$_arr[self::REQUEST_URL] = $url;
+    self::$_arr[self::REQUEST] = null;
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    $header = [];
+    if(!empty($data) && ((array)$data) > 0){
+      $postData = json_encode($data);
+      self::$_arr[self::REQUEST] = $postData;
+      curl_setopt($ch, CURLOPT_POST, 1);
+      curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+      $header[] = 'Content-Type: application/json';
+    }
+    if(self::$_token !== null) $header[] = 'Authorization: Bearer '.self::$_token;
+    self::$_arr[self::REQUEST_HEADER] = $header;
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch,CURLOPT_FOLLOWLOCATION,true);
+    self::$_arr[self::RESULT] = curl_exec($ch);
+    $result = json_decode(self::$_arr[self::RESULT]);
+    if(json_last_error() != JSON_ERROR_NONE) $result = self::$_arr[self::RESULT];
+    self::$_arr[self::RESULT_CODE] = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close ($ch);
+    self::$_arr[self::ERROR] = isset($result->errors) ? $result->errors[0]->code : false;
+    self::$_arr[self::ERROR_MSG] = isset($result->errors) ? $result->errors[0]->message : false;
+    $result_success = [200,202,204];
+    return in_array(self::$_arr[self::RESULT_CODE],$result_success) ? $result : false;
+  }
+
+  public static function get($v=null){
+    if($v === null) return new class {
+      public function error()  { return (object)['code' => Veyosis::get(Veyosis::ERROR),      'desc' => Veyosis::get(Veyosis::ERROR_MSG)]; }
+      public function request(){ return (object)['url'  => Veyosis::get(Veyosis::REQUEST_URL),'body' => Veyosis::get(Veyosis::REQUEST)]; }
+      public function result() { return (object)['code' => Veyosis::get(Veyosis::RESULT_CODE),'body' => Veyosis::get(Veyosis::RESULT)]; }
+    };
+    return isset(self::$_arr[$v]) ? self::$_arr[$v] : false;
+  }
+}
